1a791ec8687e8195df1815e84f2050294924bdcf
Dawid Rogowicz <rogowiczdawid@gmail.com>
2018-02-01 20:29:49 +0100

    78% accuracy model

--- model.py
+++ model.py
@@ -3,16 +3,16 @@ from tflearn import input_data, conv_2d, max_pool_2d, fully_connected, regressio
 
 def model(size=32, color_channels=3):
     convnet = input_data((None, size, size, color_channels))
-    convnet = conv_2d(convnet, 16, 5, activation='relu')
-    convnet = conv_2d(convnet, 16, 5, activation='relu')
+    convnet = conv_2d(convnet, 12, 5, activation='relu')
+    convnet = conv_2d(convnet, 12, 5, activation='relu')
     convnet = max_pool_2d(convnet, 2)
     convnet = conv_2d(convnet, 20, 3, activation='relu')
-    convnet = max_pool_2d(convnet, 2)
     convnet = conv_2d(convnet, 20, 3, activation='relu')
     convnet = max_pool_2d(convnet, 2)
     convnet = fully_connected(convnet, 1024, 'relu')
+    convnet = fully_connected(convnet, 512, 'relu')
     convnet = dropout(convnet, .9)
     convnet = fully_connected(convnet, 10, 'softmax')
     convnet = regression(convnet)
 
-    return DNN(convnet, tensorboard_verbose=3)
+    return DNN(convnet, tensorboard_verbose=1, tensorboard_dir='tflearn_logs')
