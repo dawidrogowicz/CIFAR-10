b12dc1a925a49f6e50391dd7f243870daf63414a
Dawid Rogowicz <rogowiczdawid@gmail.com>
2018-02-03 18:13:23 +0100

    80% accuracy

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-from tflearn import input_data, conv_2d, max_pool_2d, fully_connected, regression, dropout, DNN
+from tflearn import input_data, conv_2d, max_pool_2d, fully_connected, regression, dropout, DNN, Adam
+adam = Adam(beta2=.998)
 
 
 def model(size=32, color_channels=3):
@@ -6,13 +7,13 @@ def model(size=32, color_channels=3):
     convnet = conv_2d(convnet, 12, 5, activation='relu')
     convnet = conv_2d(convnet, 12, 5, activation='relu')
     convnet = max_pool_2d(convnet, 2)
-    convnet = conv_2d(convnet, 20, 3, activation='relu')
-    convnet = conv_2d(convnet, 20, 3, activation='relu')
+    convnet = conv_2d(convnet, 18, 3, activation='relu')
+    convnet = conv_2d(convnet, 16, 3, activation='relu')
     convnet = max_pool_2d(convnet, 2)
     convnet = fully_connected(convnet, 1024, 'relu')
-    convnet = fully_connected(convnet, 512, 'relu')
-    convnet = dropout(convnet, .9)
+    convnet = fully_connected(convnet, 256, 'relu')
+    convnet = dropout(convnet, .95)
     convnet = fully_connected(convnet, 10, 'softmax')
-    convnet = regression(convnet)
+    convnet = regression(convnet, optimizer=adam)
 
     return DNN(convnet, tensorboard_verbose=1, tensorboard_dir='tflearn_logs')
